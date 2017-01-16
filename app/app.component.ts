f66854ff1735ac862db6478c75c240df0d1b128f
Tony Saavedra <ajsaavedrax@gmail.com>
2017-01-15 23:02:56 -0800

    Initializing repo with starter files

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="/" class="navbar-brand">Welcome to ngBookSearch</a>
+                </div>
+            </nav>
+        </header>
+    `
+})
+
+export class AppComponent{}
